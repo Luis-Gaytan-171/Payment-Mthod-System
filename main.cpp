82df90dede7870a63b1e0ad1e628dab58d12737a
Laura Thomas <peluchethomasgodos@gmail.com>
2025-05-22 20:47:19 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,12 @@ using namespace std;
 
 int main(){
 
+    string date= "22/05/2025";
+
     cout<<"Welcome to our payment system!"<<endl;
 
     float prodPrice; 
-    cout<<"Enter the product of your price: ";
+    cout<<"Enter the price of your product: ";
 
     cin>>prodPrice; 
 
@@ -20,11 +22,43 @@ int main(){
 
     cin>>amountPaid;
 
-    cout<<"Are you paying with cash or card?";
+    cout<<"Are you paying with cash or card? ";
 
     string cashOrCard; 
 
-    getline (cin, cashOrCard);
+    getline (cin >> std::ws, cashOrCard);
+ 
+
+    if (cashOrCard=="card" || cashOrCard=="Card"|| cashOrCard =="CARD"){
+
+        cout<<"Enter the name of your bank: "<<endl;
+        string bankName;
+        getline (cin>> ws, bankName);
+        cout<<bankName<<endl; 
+        cout << "Enter your card number: ";
+        string cardNum;
+        getline (cin>>ws, cardNum);
+        Card card(amountPaid,date,prodPrice, bankName, cardNum);
+        System system;
+        system.addCardPayment(card);
+
+        cout<<card.getInfo()<<endl;
+   
+
+    }
+
+    else if (cashOrCard=="cash" || cashOrCard=="Cash"|| cashOrCard =="CASH"){
+
+        Cash cash(amountPaid, date, prodPrice);
+        System system; 
+        system.addCashPayment(cash);
+
+        cout<<cash.getInfo()<<endl; 
+
+    }
+
+    
+
 
     return 0;
 }
